2d86c27185d357875c29a9820b9d241507b17f62
AllySu <18795397123@163.com>
2018-08-04 13:18:01 +0800

    change the name of origin file

---
+++ tencent-service/server/controllers/demo.js
@@ -0,0 +1,7 @@
+module.exports = ctx => {
+  ctx.state.data = {
+    msg: 'Hello World，这些json数据是要返回给前端的，post或get拿到数据，服务器就是处理数据的88www2222111别再说，地铁太挤工作不易，你可曾见过火车上肩扛手提，许多我们叫爸的人拖着比自己都高的行李奔赴生计压力太大生活艰辛，你可曾知道有人已经年逾古稀依然起早贪黑，只为解决一家温饱！灯红酒绿索然无味，你可曾想过穿梭城市的民工也许连一瓶矿泉水都狠拥有心珍惜！',
+    num:2018,
+    success:{father:"ally",mather:"beautiful",age:26,tall:175}
+  }
+}
--- wafer2-quickstart-nodejs/server/controllers/demo.js
+++
@@ -1,5 +0,0 @@
-module.exports = ctx => {
-  ctx.state.data = {
-    msg: 'Hello World，这些json数据是要返回给前端的，post或get拿到数据，服务器就是处理数据的88'
-  }
-}
